611e30dd16a7e08dadd7d3647e9bf43ea9043458
Josh Spicer <jospicer@microsoft.com>
2021-04-08 12:30:03 -0400

    test dotnet app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kitchensink_personal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
