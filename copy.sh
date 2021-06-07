a101f0be4cd2ea944209d9b41d1a99b8b5671980
Josh Spicer <jospicer@microsoft.com>
2021-06-07 15:49:44 -0400

    jun 7 2021

--- copy.sh
+++ copy.sh
@@ -2,5 +2,4 @@
 rm -rf .devcontainer
 rm -rf test-project
 
-cp -r ../vscode-dev-containers/containers/codespaces-linux/.devcontainer .
-cp -r ../vscode-dev-containers/containers/codespaces-linux/test-project .
+cp -r ../vscode-dev-containers/containers/codespaces-linux/ .
